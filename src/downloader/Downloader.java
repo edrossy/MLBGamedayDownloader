98d43459b36c6f99d8d73712b40dbd02983b2f41
edrossy <edw.rossy@gmail.com>
2018-10-04 01:23:56 -0500

    Update Downloader.java
    Some fixes applied so that is now works with latest changes from MLBAM.

--- src/downloader/Downloader.java
+++ src/downloader/Downloader.java
@@ -1,4 +1,3 @@
-package downloader;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -9,7 +8,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.SystemUtils;
@@ -22,14 +20,73 @@ import org.jsoup.select.Elements;
 
 public class Downloader {
 	
+	class FileDownloader implements Callable<String> {
+		URL url;
+		File file;
+		FileDownloader(URL u, File f) {
+			url = u;
+			file = f;
+		}
+
+		public String call() {
+			try {
+				FileUtils.copyURLToFile(url, file, 5000, 5000);
+			} catch (IOException e) {
+				if (file.getName().equalsIgnoreCase("game_events.json") || file.getName().equalsIgnoreCase("boxscore.json")) {
+					URL u = null;
+					try {
+						u = new URL("http://"+url.getHost()+url.getPath().replace("json", "xml"));
+						File f = new File(getOSPath(String.format("%s\\%s",baseLocalURL, u.getFile())));
+						print(u+" "+f);
+						FileUtils.copyURLToFile(u, f, 5000, 5000);
+					} catch (IOException e1) {
+						s.release();
+						return null;
+					}
+				}
+				else if (file.getName().equalsIgnoreCase("inning_all.xml")) {
+					try {
+						int i = 1;
+						boolean active = true;
+						while (active) {
+							String sURL = "http://"+url.getHost()+url.getPath().replace("inning_all.xml", "inning_"+i+".xml");
+							Connection c = Jsoup.connect(sURL).header("Method", "HEAD");
+							if (c.execute().statusCode() == 200) {
+								i++;
+								URL u = new URL(sURL);
+								File f = new File(getOSPath(baseLocalURL + "\\" + u.getFile()));
+								FileUtils.copyURLToFile(u, f, 5000, 5000);
+							}
+							else {
+								active = false;
+							}
+						}
+					} catch (IOException e1) {
+						//We expect this exception to throw since we just count up from 1, and the amount of innings
+						//in a game is usually 9 so it will throw when it runs out of innings. This is just to make
+						//it easier by not having to figure out how many innings were actually in the game
+						s.release();
+						return null;
+					}
+					
+				}
+				else {
+				}
+				s.release();
+				return null;
+			}
+			s.release();
+			return file.getAbsolutePath();
+		}
+	}
 	static String baseURL = "http://gd2.mlb.com/components/game/mlb";
 	static String baseLocalURL;
 	static ArrayList<String> files = new ArrayList<String>();
 	static int maxThreads = 8;
-	Semaphore s;
 	
-	private static void print(String s) {
-		System.out.println(s);
+	private static String getOSPath(String path) {
+		if (SystemUtils.IS_OS_MAC) return path.replaceAll("\\", "/");
+		else return path;
 	}
 	
 	public static void main(String[] args) {
@@ -73,19 +130,25 @@ public class Downloader {
 		downloader.download(year);
 	}
 	
+	private static void print(String s) {
+		System.out.println(s);
+	}
+
+	Semaphore s;
+	
 	public void download(int year) {
 		print("Starting with "+maxThreads+" threads");
 		s = new Semaphore(maxThreads);
-		for(int month = 4 ; month <= 11 ; month++) {
+		for(int month = 2 ; month <= 11 ; month++) {
 			download(year, month);
 		}
 	}
-
+	
 	public void download(int year, int month) {
 		try {
 			ExecutorService executor = Executors.newCachedThreadPool();
 			for( int day = 1 ; day <= 31 ; day++) {
-				String parentFolder = String.format("%s/year_%02d/month_%02d/day_%02d/",
+				String parentFolder = String.format("%s/year_%02d/month_%02d/day_%02d",
 						baseURL,
 						year,
 						month,
@@ -93,13 +156,15 @@ public class Downloader {
 
 				ArrayList<String> gameFolders = new ArrayList<String>();
 				try {
-					Document doc = Jsoup.connect(parentFolder).post();
+					Document doc = Jsoup.connect(parentFolder).get();
 					Document parse = Jsoup.parse(doc.html());
 					Elements lis = parse.select("li > a");
+
 					for (Element li : lis) {
 						String href = li.attr("href");
-						if (href.startsWith("gid_")) {
-							gameFolders.add(href.replace("/", ""));
+
+						if (href.contains("/gid_")) {
+							gameFolders.add(href.substring(7));
 						}
 					}
 				} catch(IOException e) {
@@ -114,15 +179,15 @@ public class Downloader {
 							month,
 							day,
 							gameFolder);
-					
+					print(directory);
 					for (String filename : files) {
 						URL u;
 						if (filename.contains("inning_")) {
 							
-							u = new URL(String.format("%s/%s/%s", directory, "inning", filename));
+							u = new URL(String.format("%s%s/%s", directory, "inning", filename));
 						}
 						else {
-							u = new URL(String.format("%s/%s", directory, filename));
+							u = new URL(String.format("%s%s", directory, filename));
 						}
 
 						File file = new File(getOSPath(String.format("%s\\%s",baseLocalURL, u.getFile())));
@@ -139,70 +204,5 @@ public class Downloader {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	class FileDownloader implements Callable<String> {
-		URL url;
-		File file;
-		FileDownloader(URL u, File f) {
-			url = u;
-			file = f;
-		}
-
-		@Override
-		public String call() {
-			try {
-				FileUtils.copyURLToFile(url, file, 5000, 5000);
-			} catch (IOException e) {
-				if (file.getName().equalsIgnoreCase("game_events.json") || file.getName().equalsIgnoreCase("boxscore.json")) {
-					URL u = null;
-					try {
-						u = new URL("http://"+url.getHost()+url.getPath().replace("json", "xml"));
-						File f = new File(getOSPath(String.format("%s\\%s",baseLocalURL, u.getFile())));
-						FileUtils.copyURLToFile(u, f, 5000, 5000);
-					} catch (IOException e1) {
-						s.release();
-						return null;
-					}
-				}
-				else if (file.getName().equalsIgnoreCase("inning_all.xml")) {
-					try {
-						int i = 1;
-						boolean active = true;
-						while (active) {
-							String sURL = "http://"+url.getHost()+url.getPath().replace("inning_all.xml", "inning_"+i+".xml");
-							Connection c = Jsoup.connect(sURL).header("Method", "HEAD");
-							if (c.execute().statusCode() == 200) {
-								i++;
-								URL u = new URL(sURL);
-								File f = new File(getOSPath(baseLocalURL + "\\" + u.getFile()));
-								FileUtils.copyURLToFile(u, f, 5000, 5000);
-							}
-							else {
-								active = false;
-							}
-						}
-					} catch (IOException e1) {
-						//We expect this exception to throw since we just count up from 1, and the amount of innings
-						//in a game is usually 9 so it will throw when it runs out of innings. This is just to make
-						//it easier by not having to figure out how many innings were actually in the game
-						s.release();
-						return null;
-					}
-					
-				}
-				else {
-				}
-				s.release();
-				return null;
-			}
-			s.release();
-			return file.getAbsolutePath();
-		}
-	}
-	
-	private static String getOSPath(String path) {
-		if (SystemUtils.IS_OS_MAC) return path.replaceAll("\\", "/");
-		else return path;
 	}
 }
